2035ef946c737c73e0e4067e8eb31202d2dc65c6
Jason-pnt <ci@pntsports.cn>
2020-08-14 19:34:54 +0800

    update

--- checkIndex.py
+++ checkIndex.py
@@ -32,7 +32,8 @@ time.sleep(15)
 driver.get('https://tools.keycdn.com/geo')
 cityName = (driver.find_element_by_xpath('//*[@id="geoResult"]/div[1]/dl[1]/dd[1]').text)
 zipcode = (driver.find_element_by_xpath('//*[@id="geoResult"]/div[1]/dl[1]/dd[3]').text)
-print('Now test begining city name: ' + cityName + ' zip code: '+ zipcode + '\n\n')
+country = (driver.find_element_by_xpath('//*[@id="geoResult"]/div[1]/dl[1]/dd[4]').text)
+print('Now test begining country: ' + country + ' city: ' + cityName + ' Zipcode: '+ zipcode + '\n\n')
 
 driver.execute_script("document.body.style.zoom='0.9'")
 
