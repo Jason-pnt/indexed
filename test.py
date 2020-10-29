3165cacebf5acf4e8ef8f39b8ad2618bd990756a
jason-pnt <ci@pntsports.cn>
2020-10-29 10:41:55 +0800

    update

--- test.py
+++ test.py
@@ -86,6 +86,7 @@ for keys in col1:
             indexed='N'
         soup=''
         driver.get('https://www.amazon.com/' + bandlinkStr + '&language=en_US')
+        time.sleep(3)
         count = driver.find_element_by_xpath('//*[@id="search"]/span/div/span/h1/div/div[1]/div/div/span[1]').text
         count = count.split(' ')[-3].replace(',','')
         soup = BeautifulSoup(driver.page_source, "html.parser")
